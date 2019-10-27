a35d7d5d74013dd9dc657edc5eff525bfb263f32
Anthony Ding <anthonyding@berkeley.edu>
2019-10-27 03:36:47 -0700

    Added base files

---
+++ fordable/fordable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fordable
+//
+//  Created by Anthony Ding on 10/26/19.
+//  Copyright © 2019 anthonyd.in. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
